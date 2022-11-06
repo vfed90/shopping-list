8fc413f7733332171704ba25e6b37719c43e5d8a
vfed90 <fed_v90@mail.ru>
2022-11-06 14:07:31 +0300

    add homework 4

--- script.js
+++ script.js
@@ -7,3 +7,24 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const input = document.querySelector("#input");
+const items = document.querySelector("#items");
+
+input.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    const itemsText = input.value;
+
+    const item = document.createElement("div");
+    item.textContent = itemsText;
+
+    if (itemsText != "") {
+      items.append(item);
+      input.value = "";
+    }
+
+    item.addEventListener("click", function () {
+      item.classList.toggle("done");
+    });
+  }
+});
